eb1da2ce9dc6db64348d9522100e44a35642c235
AleksySYS <Aleksy.adamczyk1996@gmail.com>
2022-09-13 19:20:26 +0200

    MapApp

---
+++ Map/Map1.py
@@ -0,0 +1,15 @@
+import folium
+map = folium.Map(location= [38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
+
+fg = folium.FeatureGroup(name="My Map")
+
+
+for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+
+map.add_child(fg)
+
+map.save("Map1.html")
+
+
+
