36402e22f481612810abdb83d2ea5bcf51d0337d
AleksySYS <Aleksy.adamczyk1996@gmail.com>
2022-09-14 18:59:40 +0200

    MapApp

--- Map/Map1.py
+++ Map/Map1.py
@@ -4,15 +4,16 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 
 
-map = folium.Map(location= [38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
+map = folium.Map(location= [38.58, -99.09], zoom_start=5, tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 
 
-for lt, ln  in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup="Volcano", icon=folium.Icon(color='green')))
+for lt, ln, el  in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+ " m", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 map.save("Map1.html")
