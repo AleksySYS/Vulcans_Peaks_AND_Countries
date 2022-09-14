da3dc2d267815bb9e318335284a4a08bad946f71
AleksySYS <Aleksy.adamczyk1996@gmail.com>
2022-09-14 18:16:25 +0200

    MAPapp

--- Map/Map1.py
+++ Map/Map1.py
@@ -1,11 +1,18 @@
 import folium
-map = folium.Map(location= [38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
+import pandas
 
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
+
+
+map = folium.Map(location= [38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 
 
-for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln  in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="Volcano", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
