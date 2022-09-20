16161c2615f8a2cacb308eef78312fe285ac8767
AleksySYS <Aleksy.adamczyk1996@gmail.com>
2022-09-20 19:54:18 +0200

    MapAPP

--- Map/Map1.py
+++ Map/Map1.py
@@ -14,14 +14,24 @@ def color_producer(elevation):
         return 'orange'
     else:
         return 'red'
-map = folium.Map(location= [38.58, -99.09], zoom_start=5, tiles="Stamen Terrain")
-fg = folium.FeatureGroup(name="My Map")
+        
+map = folium.Map(location= [38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 
 for lt, ln, el  in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+ " m", icon=folium.Icon(color=color_producer(el))))
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el)+ " m",
+    fill_color=color_producer(el),fill= True, fill_opacity=0.7))
 
-map.add_child(fg)
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 
+else 'blue' if 10000000 <= x['properties']['POP2005'] < 70000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
 
 
